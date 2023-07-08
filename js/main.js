251c8e823baa6cae9c59d079acc2761d450620a1
DanteCarrizo <dantecarrizoph@gmail.com>
2023-07-08 17:29:26 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,50 +1,56 @@
 const shopContent = document.getElementById("shopContent");
 const verCarrito = document.getElementById("verCarrito");
-const modalContainer = document.getElementById("modal-container")
-const cantidadCarrito = document.getElementById("cantidadCarrito")
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 let carrito = JSON.parse(localStorage.getItem("Cart")) || [];
 
-productos.forEach((product) => {
-  let content = document.createElement("div");
-  content.className = "card";
-  content.innerHTML = `
-    <img src="${product.img}">
-    <h3>${product.nombre}</h3> 
-    <p class="price">${product.precio} $</p>
-  `;
+const getProducts = async ()=> {
+  const response = await fetch("data.json");
+  const data = await response.json();
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+      <img src="${product.img}">
+      <h3>${product.nombre}</h3> 
+      <p class="price">${product.precio} $</p>
+    `;
+  
+    shopContent.append(content);
+  
+    let comprar = document.createElement("button")
+    comprar.innerText = "Comprar";
+    comprar.className = "Comprar";
+  
+    content.append(comprar);
+  
+    comprar.addEventListener("click", () =>{
+      
+    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+  
+    if(repeat){
+      carrito.map((prod) => {
+        if(prod.id === product.id){
+          prod.cantidad++;
+        }
+      });
+    } else{
+      carrito.push({
+        id: product.id,
+        img: product.img,
+        nombre: product.nombre,
+        precio: product.precio,
+        cantidad: product.cantidad,
+      });
+    }
+    carritoCounter();
+    saveLocal();
+    })
+  });  
+};
+getProducts();
 
-  shopContent.append(content);
-
-  let comprar = document.createElement("button")
-  comprar.innerText = "Comprar";
-  comprar.className = "Comprar";
-
-  content.append(comprar);
-
-  comprar.addEventListener("click", () =>{
-    
-  const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-
-  if(repeat){
-    carrito.map((prod) => {
-      if(prod.id === product.id){
-        prod.cantidad++;
-      }
-    });
-  } else{
-    carrito.push({
-      id: product.id,
-      img: product.img,
-      nombre: product.nombre,
-      precio: product.precio,
-      cantidad: product.cantidad,
-    });
-  }
-  carritoCounter();
-  saveLocal();
-  })
-});
 
 const saveLocal = () => {
   localStorage.setItem("Cart", JSON.stringify(carrito));
